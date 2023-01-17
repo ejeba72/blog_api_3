d47d1aec7a7d737af64cfefe7cb36139ab6adc02
Emmanuel Eni <eejeba@gmail.com>
2023-01-17 21:40:34 +0100

    refactored database connection

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { connect, connection } = require('mongoose');
 const { config } = require('dotenv');
 const { healthRoute } = require('./routes/healthRoute');
 const { generalRoute } = require('./routes/generalRoute');
@@ -9,20 +8,7 @@ config()
 
 const app = express();
 const PORT = process.env.PORT;
-const DB_URI = process.env.DB_URI;
 const apiV1 = '/api/v1';
-
-// MONGOOSE CONNECTION
-mongoDB().catch(err => console.log(err));
-
-async function mongoDB() {
-  await connect(DB_URI);
-}
-
-connection.on('connected', () => {
-  console.log('MongoDB is connected')
-})
-
 
 // MIDDLEWARE 
 app.use('/', healthRoute)
