aed78bbd8dfcf4a9ee927f7bc1136baf8c49beda
Emmanuel Eni <eejeba@gmail.com>
2023-01-19 11:57:51 +0100

    bug has finally been dectected. bug: express.json, correction: express.json()

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,14 @@
 const { User } = require("../Models/UserModel");
 
 async function getAllLogic(req, res) {
-  console.log('get users request');
-  const allUsers = await User.find();
-  res.send(allUsers);
+  try {
+    console.log('get users request');
+    const allUsers = await User.find();
+    res.send(allUsers);
+  } catch (err) {
+    console.log(err.messaage);
+    res.send(err.message);
+  }
 }
 
-// module.exports = { getAllLogic }
+module.exports = { getAllLogic }
