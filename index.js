fb1d4097324f759229dd4f5ece6f6b350ce8aebf
Emmanuel Eni <eejeba@gmail.com>
2023-02-07 23:40:02 +0100

    index.js code refactor

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-const express = require('express');
-const { config } = require('dotenv');
-const { healthRoute } = require('./routes/healthRoute');
-const { devRoute } = require('./routes/devRoute');
-const { generalRoute } = require('./routes/generalRoute');
-const { userRoute } = require('./routes/userRoute');
-const { mongoDB } = require('./database/connection');
-const { editRoute } = require('./routes/editRoute');
-
-// DOTENV CONFIG AND MONGODB CONNECTION
-config();
-mongoDB();
-
-const app = express();
-const PORT = process.env.PORT;
-const apiV1 = '/api/v1';
-
-// MIDDLEWARE 
-app.use(express.json());
-app.use('/', healthRoute);
-app.use('/dev', devRoute);  // For development purpose only
-app.use(`${apiV1}/general`, generalRoute);
-app.use(`${apiV1}/users`, userRoute);
-app.use(`${apiV1}/edit`, editRoute)
+const { app, PORT } = require("./app");
 
 app.listen(PORT, () => {
   console.log(`Server is attentively listening to request at port ${PORT}`);
